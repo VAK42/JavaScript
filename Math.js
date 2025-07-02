7c0250581345a1fa4e56563a71dc45d7f34b1688
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-07-02 16:36:39 +0700

    .

---
+++ Math.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let a = 4, b = 2;
+let c = a + b;   // Addition
+let d = a - b;   // Subtraction
+let e = a * b;   // Multiplication
+let f = a / b;   // Division
+let g = a % b;   // Remainder
+let h = a ** b;  // Exponentiation
