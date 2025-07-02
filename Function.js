15a00321b7ed53b5f75b8ad9e6e68e688a20a44e
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-07-02 17:00:22 +0700

    .

---
+++ Function.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function name(param) {
+  console.log(param);  // Function Declaration
+}
+
+let Name = function (param) {
+  console.log(param);  // Function Expression
+};
+
+let NAME = (arg) => {
+  console.log(arg);    // Arrow Function
+};
