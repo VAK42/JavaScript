9729b9660bef0b7e2b38687f445676c7a926bf0e
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-07-02 16:42:40 +0700

    .

---
+++ BitwiseOperator.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let a = 4, b = 2;
+let c = a & b;    // AND
+let d = a | b;    // OR
+let e = a ^ b;    // XOR
+let f = ~a;       // NOT
+let g = a << 1;   // LEFT SHIFT
+let h = a >> 1;   // RIGHT SHIFT
+let i = a >>> 1;  // ZERO-FILL RIGHT SHIFT
