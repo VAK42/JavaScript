d61b16f857d55f76198f537ee08bd12bd532c1bc
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-07-02 16:58:11 +0700

    .

---
+++ SwitchStatement.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let a = 'A';
+switch (a) {
+  case 'A':
+    console.log('X');
+    break;
+  case 'B':
+    console.log('Y');
+    break;
+  case 'C':
+    console.log('Z');
+    break;
+  default:
+    console.log('W');
+    break;
+}
