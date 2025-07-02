c9195aa43e783b234fe12cb08b64b61a41de3203
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-07-02 16:34:12 +0700

    .

---
+++ BasicOperator.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let a = 4, b = 2, c = 8;
+let d = c + 2;  // Operand: The Value Or Variable That An Operator Is Applied To
+let e = -c;     // Unary: A Single Operand
+let f = a + b;  // Binary: Two Operands
