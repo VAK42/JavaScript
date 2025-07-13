e116f76c4cf7d3d44ea89993ed4ecf32e9a870e9
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-07-14 01:21:21 +0700

    .

--- ErrorHandling.js
+++ ErrorHandling.js
@@ -11,18 +11,33 @@ function a(b) {
     if (b === 'x') {
       throw new ReferenceError('ReferenceError');
     }
+    if (b === 'y') {
+      throw new RangeError('RangeError');
+    }
+    if (b === 'z') {
+      throw new EvalError('EvalError');
+    }
+    if (b === 'w') {
+      throw new URIError('URIError');
+    }
     console.log('JS');
   } catch (c) {
     if (c instanceof TypeError) console.log(c.message);
     else if (c instanceof SyntaxError) console.log(c.message);
     else if (c instanceof ReferenceError) console.log(c.message);
+    else if (c instanceof RangeError) console.log(c.message);
+    else if (c instanceof EvalError) console.log(c.message);
+    else if (c instanceof URIError) console.log(c.message);
     else throw c;
   } finally {
     console.log('42');
   }
 }
 
-a(482);  // TypeError → 42
-a(' ');  // SyntaxError → 42
-a('x');  // ReferenceError → 42
-a('K');  // JS → 42
+a(482);     // TypeError → 42
+a(' ');     // SyntaxError → 42
+a('x');     // ReferenceError → 42
+a('y');     // RangeError → 42
+a('z');     // EvalError → 42
+a('w');     // URIError → 42
+a('K');     // JS → 42
