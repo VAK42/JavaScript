e1f70be0b89f945000adf8a2358dddf10ef1b5fe
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-07-14 17:56:40 +0700

    .

--- PropertyFlagDescriptor.js
+++ PropertyFlagDescriptor.js
@@ -3,6 +3,14 @@
 let a = { x: 'JS' };
 let b = Object.getOwnPropertyDescriptor(a, 'x');
 console.log(JSON.stringify(b, null, 2));
+/*
+{
+  "value": "JS",
+  "writable": true,
+  "enumerable": true,
+  "configurable": true
+}
+*/
 
 let c = {};
 Object.defineProperty(c, 'x', {
@@ -10,6 +18,14 @@ Object.defineProperty(c, 'x', {
 });
 let d = Object.getOwnPropertyDescriptor(c, 'x');
 console.log(JSON.stringify(d, null, 2));
+/*
+{
+  "value": "JS",
+  "writable": false,
+  "enumerable": false,
+  "configurable": false
+}
+*/
 
 Object.defineProperties(c, {
   y: {
@@ -33,3 +49,31 @@ Object.defineProperties(c, {
 });
 let e = Object.getOwnPropertyDescriptors(c);
 console.log(JSON.stringify(e, null, 2));
+/*
+{
+  "x": {
+    "value": "JS",
+    "writable": false,
+    "enumerable": false,
+    "configurable": false
+  },
+  "y": {
+    "value": "A",
+    "writable": false,
+    "enumerable": false,
+    "configurable": false
+  },
+  "z": {
+    "value": "B",
+    "writable": false,
+    "enumerable": true,
+    "configurable": false
+  },
+  "w": {
+    "value": "C",
+    "writable": true,
+    "enumerable": false,
+    "configurable": true
+  }
+}
+*/
