3f11b79ed9e16f550703d5ea2d0e226eff7dff47
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-07-15 17:44:05 +0700

    .

---
+++ PrototypeModernMethod.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let a ={x:4};
+let b ={y:2};
+console.log(Object.getPrototypeOf(a)===Object.prototype);  // True
+Object.setPrototypeOf(a,b);
+console.log(a.y);  // 2
+
+let c = Object.create(b);
+c.z = 8;
+console.log(c.y);  // 2
+console.log(c.z);  // 8
+
+let d = Object.create(null);
+console.log(Object.getPrototypeOf(d));  // Null
+
+let e = Object.create(Object.prototype, {
+  j:{
+    value:42,
+    writable:true,
+    enumerable:true,
+    configurable:true
+  }
+});
+console.log(e.j);  // 42
+console.log(Object.getPrototypeOf(e)===Object.prototype);  // True
